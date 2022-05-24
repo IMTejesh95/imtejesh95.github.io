9dbf47c89cbfd3b0e7de20905acba567a9710f87
Tejesh Chauragade <chauragade.tejesh@gmail.com>
2022-05-24 21:22:40 +0530

    add exportPath map in next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  trailingSlash: true,
+  exportPathMap: async function (_defaultPathMap, { _dev, _dir, _outDir, _distDir, _buildId }) {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/projects': { page: '/projects' },
+      '/tech-stack': { page: '/tech-stack' },
+      '/resume': { page: '/resume' }
+    };
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
