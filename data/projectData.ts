3aca1bad087d760252cf085029f9d582350bf673
Tejesh Chauragade <chauragade.tejesh@gmail.com>
2022-05-25 09:51:58 +0530

    feat: add an academic project

--- data/projectData.ts
+++ data/projectData.ts
@@ -28,5 +28,15 @@ export const projectsList = [
     description:
       'Python web-scrapper to inform % alter in stock price, can be planned as cron work.',
     techStack: ['Python', 'BeautifulSoup4', 'WebScraping', 'cron']
+  },
+  {
+    title: 'Online Banking System (E-Banking)',
+    imageLight: '/assets/images/projects/cover/ebanking.jpg',
+    blurHash: 'L4ADc400P*Zi4Tu1y;Qo00pH#YXl',
+    site: 'https://github.com/IMTejesh95/online-banking-sunbeam',
+    gitHub: 'https://github.com/IMTejesh95/online-banking-sunbeam',
+    description:
+      'An app to automate bank processing and facilitate online banking. It helps the users to easily access the bank facilities such as checking balance, transferring funds, etc, and staff to approve and manage accounts.',
+    techStack: ['Java', 'Spring', 'Hibernate', 'MySQL', 'JavaScript']
   }
 ];
